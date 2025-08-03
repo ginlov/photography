798ca024f15b43073a9193d7d245df705f87bdc6
Vu Long Giang <leo@Vus-MacBook-Pro.local>
2025-08-03 16:25:40 -0700

    update

--- reduce_size.sh
+++ reduce_size.sh
@@ -1,3 +1,3 @@
 find images/thumbs -type f \( -iname "*.jpg" -o -iname "*.jpeg" \) -print0 | while IFS= read -r -d '' img; do
-  sips -s format jpeg -s formatOptions 25 "$img" --out "$img"
+  sips -s format jpeg -s formatOptions 10 "$img" --out "$img"
 done
